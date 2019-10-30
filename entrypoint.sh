d3fbd5faca929c252ae99c14cc4fc042cf5e41e9
Romain Decker <deckerromain@gmail.com>
2019-10-30 21:05:16 +0100

    More troubleshoot again

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,14 @@ ls -la public/
 
 sh -c "hugo $*"
 
-sh -c "hugo"
-
 sh -c "hugo env"
 
 sh -c "hugo -v"
+
+pwd
+
+ls -la /github/workspace/public/
+
+ls -la /github/workspace/public/posts/
 
 ls -la public/posts/
