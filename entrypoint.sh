e44848067ad12addcb902796ae6766977e2f9314
Romain Decker <deckerromain@gmail.com>
2019-10-31 09:06:04 +0100

    Fresh test this morning

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,26 +3,7 @@
 set -e
 set -x
 
-echo $GITHUB_WORKSPACE
-echo $GITHUB_REPOSITORY
-
-root_path="$GITHUB_WORKSPACE"
-echo "Root path is: ${root_path}"
-blog_path="$GITHUB_WORKSPACE/public/"
-echo "Blog path is: ${blog_path}"
-mkdir -p "$blog_path"
-mkdir -p "$root_path"
-
-touch public/test.html
-
-cd "$root_path"
-echo "Preparing to build blog"
-hugo
-echo "Building is done. Copying over generated files"
-#cp -R public/* "$blog_path"/
-echo "Copy is done...or not..."
-
-hugo deploy --dryRun
+hugo env
 
 hugo deploy
 
