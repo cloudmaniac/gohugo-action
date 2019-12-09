ce26d76b86865608f9c272f1effae069328525a5
Romain Decker <romain@cloudmaniac.net>
2019-12-09 22:48:24 +0000

    Debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,15 @@ set -x
 
 hugo env
 
+ls -al
+
+ls -al /github/workspace/public/
+
 mkdir -p /github/workspace/public/
+
+ls -al
+
+ls -la /github/workspace/public/
 
 hugo && hugo deploy --invalidateCDN --maxDeletes -1
 
