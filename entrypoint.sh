35e68e48125c95696277a0695b1d669d06cc6f09
Romain Decker <romain@cloudmaniac.net>
2020-03-22 06:58:42 +0100

    Last debug (wrt $CLOUDFRONT_DISTRIBUTION_ID)

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,24 +5,13 @@ set -x
 
 hugo env
 
-ls -al
 
 ls -al /github/workspace/
 
-ls -al /github/workspace/content/
+ls -la /github/workspace/themes/
 
-ls -al /github/workspace/content/post/
-
-mkdir -p /github/workspace/public/
-
-chmod -R 777 /github/workspace/public/
-
-ls -la
-
-ls -la /github/workspace/public/
+echo $CLOUDFRONT_DISTRIBUTION_ID
 
 hugo && hugo deploy --invalidateCDN --maxDeletes -1
-
-ls -la /github/workspace/public/
 
 exit 0
