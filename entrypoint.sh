89d6095af18b7b5aabdb5ea327023163ba5e899d
Romain Decker <romain@cloudmaniac.net>
2020-03-24 08:46:43 +0100

    Cleaning and added some comments

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,16 @@
 set -e
 set -x
 
+echo "Checking Hugo version:"
 hugo env
 
-
+echo "Validating repository that has been checked out:"
 ls -al /github/workspace/
 
+echo "Validating themes folder (usually a git submodules)"
 ls -la /github/workspace/themes/
 
-echo $CLOUDFRONT_DISTRIBUTION_ID
-
+echo "Build website and deploy"
 hugo && hugo deploy --invalidateCDN --maxDeletes -1
 
 exit 0
